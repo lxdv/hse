24608e0213822fb3f5c3be175803e687052dcb29
Alexander Lyashuk <alexander.lyashuk@xperience.ai>
2020-02-17 19:26:47 +0300

    some files

---
+++ nn-frameworks/keras/layers/attention-layer.py
@@ -0,0 +1,183 @@
+import keras
+from keras import backend as K
+
+def dot_product(x, kernel):
+    """
+    Wrapper for dot product operation, in order to be compatible with both
+    Theano and Tensorflow
+    Args:
+        x (): input
+        kernel (): weights
+    Returns:
+    """
+    if K.backend() == 'tensorflow':
+        return K.squeeze(K.dot(x, K.expand_dims(kernel)), axis=-1)
+    else:
+        return K.dot(x, kernel)
+    
+
+class AttentionWithContext(keras.layers.Layer):
+    """
+    Attention operation, with a context/query vector, for temporal data.
+    Supports Masking.
+    Follows the work of Yang et al. [https://www.cs.cmu.edu/~diyiy/docs/naacl16.pdf]
+    "Hierarchical Attention Networks for Document Classification"
+    by using a context vector to assist the attention
+    # Input shape
+        3D tensor with shape: `(samples, steps, features)`.
+    # Output shape
+        2D tensor with shape: `(samples, features)`.
+    How to use:
+    Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
+    The dimensions are inferred based on the output shape of the RNN.
+    Note: The layer has been tested with Keras 2.0.6
+    Example:
+        model.add(LSTM(64, return_sequences=True))
+        model.add(AttentionWithContext())
+        # next add a Dense layer (for classification/regression) or whatever...
+    """
+
+    def __init__(self,
+                 W_regularizer=None, u_regularizer=None, b_regularizer=None,
+                 W_constraint=None, u_constraint=None, b_constraint=None,
+                 bias=True, **kwargs):
+
+        self.supports_masking = True
+        self.init = keras.initializers.get('glorot_uniform')
+
+        self.W_regularizer = keras.regularizers.get(W_regularizer)
+        self.u_regularizer = keras.regularizers.get(u_regularizer)
+        self.b_regularizer = keras.regularizers.get(b_regularizer)
+
+        self.W_constraint = keras.constraints.get(W_constraint)
+        self.u_constraint = keras.constraints.get(u_constraint)
+        self.b_constraint = keras.constraints.get(b_constraint)
+
+        self.bias = bias
+        super(AttentionWithContext, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight((input_shape[-1], input_shape[-1],),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        if self.bias:
+            self.b = self.add_weight((input_shape[-1],),
+                                     initializer='zero',
+                                     name='{}_b'.format(self.name),
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+
+        self.u = self.add_weight((input_shape[-1],),
+                                 initializer=self.init,
+                                 name='{}_u'.format(self.name),
+                                 regularizer=self.u_regularizer,
+                                 constraint=self.u_constraint)
+
+        super(AttentionWithContext, self).build(input_shape)
+
+    def compute_mask(self, input, input_mask=None):
+        # do not pass the mask to the next layers
+        return None
+
+    def call(self, x, mask=None):
+        uit = dot_product(x, self.W)
+
+        if self.bias:
+            uit += self.b
+
+        uit = K.tanh(uit)
+        ait = dot_product(uit, self.u)
+
+        a = K.exp(ait)
+
+        # apply mask after the exp. will be re-normalized next
+        if mask is not None:
+            # Cast the mask to floatX to avoid float64 upcasting in theano
+            a *= K.cast(mask, K.floatx())
+
+        # in some cases especially in the early stages of training the sum may be almost zero
+        # and this results in NaN's. A workaround is to add a very small positive number ε to the sum.
+        # a /= K.cast(K.sum(a, axis=1, keepdims=True), K.floatx())
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        #a = K.expand_dims(a)
+        return a
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], input_shape[-1]
+
+
+
+def get_activations(model, model_inputs, print_shape_only=False, layer_name=None):
+    #print('----- activations -----')
+    activations = []
+    inp = model.input
+
+    model_multi_inputs_cond = True
+    if not isinstance(inp, list):
+        # only one input! let's wrap it in a list.
+        inp = [inp]
+        model_multi_inputs_cond = False
+
+    outputs = [layer.output for layer in model.layers if
+               layer.name == layer_name or layer_name is None]  # all layer outputs
+
+    funcs = [K.function(inp + [K.learning_phase()], [out]) for out in outputs]  # evaluation functions
+
+    if model_multi_inputs_cond:
+        list_inputs = []
+        list_inputs.extend(model_inputs)
+        list_inputs.append(0.)
+    else:
+        list_inputs = [model_inputs, 0.]
+
+    # Learning phase. 0 = Test mode (no dropout or batch normalization)
+    # layer_outputs = [func([model_inputs, 0.])[0] for func in funcs]
+    layer_outputs = [func(list_inputs)[0] for func in funcs]
+    for layer_activations in layer_outputs:
+        activations.append(layer_activations)
+#         if print_shape_only:
+#             print(layer_activations.shape)
+#         else:
+#             print(layer_activations)
+    return activations
+
+
+def display_activations(activation_maps):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    """
+    (1, 26, 26, 32)
+    (1, 24, 24, 64)
+    (1, 12, 12, 64)
+    (1, 12, 12, 64)
+    (1, 9216)
+    (1, 128)
+    (1, 128)
+    (1, 10)
+    """
+    batch_size = activation_maps[0].shape[0]
+    #assert batch_size == 1, 'One image at a time to visualize.'
+    for i, activation_map in enumerate(activation_maps):
+        #print('Displaying activation map {}'.format(i))
+        shape = activation_map.shape
+        if len(shape) == 4:
+            activations = np.hstack(np.transpose(activation_map[0], (2, 0, 1)))
+        elif len(shape) == 2:
+            # try to make it square as much as possible. we can skip some activations.
+            activations = activation_map[0]
+            num_activations = len(activations)
+            if num_activations > 1024:  # too hard to display it on the screen.
+                square_param = int(np.floor(np.sqrt(num_activations)))
+                activations = activations[0: square_param * square_param]
+                activations = np.reshape(activations, (square_param, square_param))
+            else:
+                activations = np.expand_dims(activations, axis=0)
+#         else:
+#             raise Exception('len(shape) = 3 has not been implemented.')
+        plt.imshow(activations, interpolation='None', cmap='coolwarm')
+        plt.show()
